c5bc1f2f4170ed3c3471adfa5348bb8ed5c53a95
Ihor Koliasa <ihor@koliasa.com>
2024-03-01 17:56:52 +0200

    ws

--- install_app.sh
+++ install_app.sh
@@ -14,6 +14,11 @@ sudo apt install -y \
 sudo apt install snapd
 sudo snap install asciiquarium
 
+#Video
+sudo apt install ubuntu-restricted-extras
+sudo apt install mpv
+sudo apt install vlc
+
 #Міняємо метов переключення роскладки кравіатури (зміна мови вводу) на ліві клавіші ALT+TAB
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt_L']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
