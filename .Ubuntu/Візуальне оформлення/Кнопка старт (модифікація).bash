38ccafb0bba46cafe84d41e8e5867446d529a5cb
Ihor Koliasa <ihor@koliasa.com>
2025-05-18 10:01:40 +0000

    Update Кнопка старт (модифікація).bash

--- .Ubuntu/Візуальне оформлення/Кнопка старт (модифікація).bash
+++ .Ubuntu/Візуальне оформлення/Кнопка старт (модифікація).bash
@@ -1,2 +1,9 @@
 sudo cp cd /home/k1/Завантажене/view-app-grid-symbolic.svg /usr/share/icons/Yaru/scalable/actions/view-app-grid-symbolic.svg
 sudo mc cd /usr/share/icons/Yaru/scalable/actions
+
+sudo bash -c '
+cp /usr/share/icons/Yaru/scalable/actions/view-app-grid-ubuntu-symbolic.svg \
+   /usr/share/icons/Yaru/scalable/actions/view-app-grid-ubuntu-symbolic.svg.bak &&
+cp /home/k1/Downloads/view-app-grid-ubuntu-symbolic.svg \
+   /usr/share/icons/Yaru/scalable/actions/view-app-grid-ubuntu-symbolic.svg &&
+gtk-update-icon-cache -f /usr/share/icons/Yaru'
